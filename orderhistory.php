747d87beb3f1f659dbdd8c8f692be9d3e8355853
francmalo <frncsmalombe7328@gmail.com>
2024-05-09 19:08:38 +0300

    changes to orderhistory

--- orderhistory.php
+++ orderhistory.php
@@ -27,10 +27,22 @@ if ($stmt->execute()) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order History</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <title>Bellaria - a Delicious Cakes and Bakery HTML Template | Shopping Cart</title>
+
+    <!-- Stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
     <style>
     body {
         padding: 20px;
@@ -42,13 +54,59 @@ if ($stmt->execute()) {
         text-align: center;
     }
 
-    .table {
-        margin-top: 20px;
+    .cart-table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 20px;
     }
 
-    .table th,
-    .table td {
-        vertical-align: middle;
+    .cart-table th,
+    .cart-table td {
+        padding: 10px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .cart-table th {
+        background-color: #f2f2f2;
+    }
+
+    .product-thumbnail img {
+        max-width: 80px;
+    }
+
+    .product-quantity .quantity {
+        display: inline-block;
+    }
+
+    .product-quantity .qty {
+        width: 60px;
+        text-align: center;
+    }
+
+    .cart-options {
+        margin-bottom: 20px;
+    }
+
+    .apply-coupon {
+        display: flex;
+        align-items: center;
+    }
+
+    .apply-coupon .form-group {
+        margin-right: 10px;
+    }
+
+    .totals-table {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .totals-table li {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 10px;
     }
 
     .btn-primary {
@@ -70,45 +128,63 @@ if ($stmt->execute()) {
 <body>
     <h1>Order History</h1>
 
-    <?php if ($result->num_rows > 0): ?>
-    <table class="table table-striped table-bordered">
-        <thead class="thead-dark">
-            <tr>
-                <th>Order ID</th>
-                <th>Order Date</th>
-                <th>Delivery Date</th>
-                <th>Delivery Type</th>
-                <th>Address</th>
-                <th>Phone</th>
-                <th>Order Status</th>
-                <th>Invoice Status</th>
-                <th>Total Amount</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $row['order_id']; ?></td>
-                <td><?php echo $row['order_date']; ?></td>
-                <td><?php echo $row['delivery_date']; ?></td>
-                <td><?php echo ($row['delivery_type'] == 1) ? 'Standard Delivery' : 'Express Delivery'; ?></td>
-                <td><?php echo $row['address']; ?></td>
-                <td><?php echo $row['phone']; ?></td>
-                <td><?php echo getOrderStatusLabel($row['order_status']); ?></td>
-                <td><?php echo getInvoiceStatusLabel($row['invoice_status']); ?></td>
-                <td>$<?php echo number_format($row['total_amount'], 2); ?></td>
-                <td>
-                    <a href="order_details.php?order_id=<?php echo $row['order_id']; ?>" class="btn btn-primary">View
-                        Details</a>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-    <?php else: ?>
-    <p class="text-center">No orders found.</p>
-    <?php endif; ?>
+    <!--Cart Section-->
+    <section class="cart-section">
+        <div class="auto-container">
+            <!--Cart Outer-->
+            <div class="cart-outer">
+                <div class="table-outer">
+                    <table class="cart-table">
+                        <thead class="cart-header">
+                            <tr>
+                                <th class="product-thumbnail">&nbsp;</th>
+                                <th class="product-name">Order ID</th>
+                                <th class="product-message">Order Date</th>
+                                <th class="product-price">Delivery Date</th>
+                                <th class="product-quantity">Delivery Type</th>
+                                <th class="product-subtotal">Address</th>
+                                <th class="product-remove">Phone</th>
+                                <th class="product-remove">Order Status</th>
+                                <th class="product-remove">Invoice Status</th>
+                                <th class="product-remove">Total Amount</th>
+                                <th class="product-remove">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if ($result->num_rows > 0): ?>
+                            <?php while ($row = $result->fetch_assoc()): ?>
+                            <tr class="cart-item">
+                                <td class="product-thumbnail">&nbsp;</td>
+                                <td class="product-name"><?php echo $row['order_id']; ?></td>
+                                <td class="product-message"><?php echo $row['order_date']; ?></td>
+                                <td class="product-price"><?php echo $row['delivery_date']; ?></td>
+                                <td class="product-quantity">
+                                    <?php echo ($row['delivery_type'] == 1) ? 'Standard Delivery' : 'Express Delivery'; ?>
+                                </td>
+                                <td class="product-subtotal"><?php echo $row['address']; ?></td>
+                                <td class="product-remove"><?php echo $row['phone']; ?></td>
+                                <td class="product-remove"><?php echo getOrderStatusLabel($row['order_status']); ?></td>
+                                <td class="product-remove"><?php echo getInvoiceStatusLabel($row['invoice_status']); ?>
+                                </td>
+                                <td class="product-remove">$<?php echo number_format($row['total_amount'], 2); ?></td>
+                                <td class="product-remove">
+                                    <a href="order_details.php?order_id=<?php echo $row['order_id']; ?>"
+                                        class="btn btn-primary">View
+                                        Details</a>
+                                </td>
+                            </tr>
+                            <?php endwhile; ?>
+                            <?php else: ?>
+                            <tr>
+                                <td colspan="11" class="text-center">No orders found.</td>
+                            </tr>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <?php
     // Helper functions to get order status and invoice status labels
@@ -140,6 +216,17 @@ if ($stmt->execute()) {
         }
     }
     ?>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fancybox.js"></script>
+    <script src="js/owl.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/select2.min.js"></script>
+    <script src="js/sticky_sidebar.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 
 </html>
